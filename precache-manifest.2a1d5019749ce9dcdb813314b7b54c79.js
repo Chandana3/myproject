4ffe0a8d00eaae96c209ff274440e515d9684d76
Chandana S <schand27@in.ibm.com>
2019-08-21 12:05:33 +0530

    Updates

---
+++ precache-manifest.2a1d5019749ce9dcdb813314b7b54c79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cfee5ca406553ed4d5e9e6c14d561f",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "7f41c131a8406bdc2cfc",
+    "url": "/myproject/static/css/main.456b4a74.chunk.css"
+  },
+  {
+    "revision": "2c4f7e66182366b304ae",
+    "url": "/myproject/static/js/2.7d6d5593.chunk.js"
+  },
+  {
+    "revision": "7f41c131a8406bdc2cfc",
+    "url": "/myproject/static/js/main.80865fed.chunk.js"
+  },
+  {
+    "revision": "9d377e21065c52675f0e",
+    "url": "/myproject/static/js/runtime~main.085e34b5.js"
+  }
+]);
